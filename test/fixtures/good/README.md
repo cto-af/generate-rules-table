a709a9549f15b54cbd03b8db3b422c3624f43294
Joe Hildebrand <joe-github@cursive.net>
2024-10-07 15:03:18 -0600

    Add tests

---
+++ test/fixtures/good/README.md
@@ -0,0 +1,11 @@
+Test generation:
+
+<!-- Rule Table Start -->
+| **Rule Name** | **Description** | **Recommended** |
+| :- | :- | :-: |
+| [`foo`](./docs/rules/foo.md) | Foo test | yes |
+| [`bar`](./docs/rules/bar.md) | Bar test | no |
+
+<!-- Rule Table End -->
+
+With text after.
--- test/index.test.js
+++ test/index.test.js
@@ -1,8 +1,31 @@
-import assert from 'node:assert';
 import {createRulesTable} from '../lib/index.js';
+import {fileURLToPath} from 'node:url';
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import {rejects} from 'node:assert';
+import snap from 'snappy-snaps';
 // eslint-disable-next-line n/no-unsupported-features/node-builtins
 import test from 'node:test';
 
-test('index', () => {
-  // assert.equal(foo(), 2);
+const fixtures = fileURLToPath(new URL('./fixtures/', import.meta.url));
+const good = path.resolve(fixtures, 'good');
+const bad = path.resolve(fixtures, 'bad');
+
+test('createRulesTable', async() => {
+  await createRulesTable({cwd: good});
+  await snap('good', await fs.readFile(path.join(good, 'README.md'), 'utf8'));
+});
+
+test('bad README', async() => {
+  await rejects(
+    () => createRulesTable({cwd: bad}),
+    {message: 'Invalid markdown file, needs start/end comments'}
+  );
+});
+
+test('bad package.json', async() => {
+  await rejects(
+    () => createRulesTable({cwd: '/'}),
+    {message: 'Unable to find package.json from "/"'}
+  );
 });
